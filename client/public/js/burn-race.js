45fe207174155692074a9f8c8622e6731a5fd0c3
chentingdong <chentingdong@gmail.com>
2015-10-01 00:18:07 -0700

    migrate race UI finished

--- client/public/js/burn-race.js
+++ client/public/js/burn-race.js
@@ -1,3 +1,106 @@
-var a = 1;
+$(document).ready(function () {
+	/* count down clock */
+	$('.countdown').countdown('2015/10/10', function(event) {
+		$(this).find('.days').text(event.strftime('%D'))
+		$(this).find('.hours').text(event.strftime('%H'))
+		$(this).find('.minutes').text(event.strftime('%M'))
+		$(this).find('.seconds').text(event.strftime('%S'))
+	});
 
-console.log(a);
+	/* carousel */
+	$('.carousel').carousel({
+		interval: 5000
+	});
+	$('.carousel').swipe({
+		swipeLeft: function() {
+			$(this).carousel('next');
+		},
+		swipeRight: function(){
+			$(this).carousel('prev');
+		}
+	})
+
+	/* smooth scroll */
+	$('a.scroll').click(function(){
+		var scrollTop = Number($( $.attr(this, 'href') ).offset().top) + Number($(this).attr('data-scroll-adjust')) + $('#nav').height();
+		$('html, body').animate({
+			scrollTop: scrollTop
+		}, 500);
+		return false;
+	});
+
+	/* accordian */
+	$('#faq ul li').click(function(){
+		$(this).find('i').toggleClass('fa-plus-circle fa-minus-circle')
+	});
+
+	/* hero image background */
+	$('.center-crop').centerCrop();
+
+	/* ajax submit contact form */
+	$('#contact-form').submit(function(e){
+		e.preventDefault();
+		$.ajax({
+			type: "POST",
+			url: $('#contact-form').attr('action'),
+			dataType: "json",
+			data: $('#contact-form').serialize(),
+			success: function(response){
+				if (response.cap == true) {
+					if (response.mail == true) {
+						msg = "Mail sent";
+						$('#contact-form')[0].reset();
+					} else {
+						msg = "We are sorry, but there appears to be a problem with the form you submitted."
+					}
+
+					$('.captcha-status').removeClass('error').text(msg)
+					.show().delay(5000).fadeOut('slow');
+				} else {
+					$('.captcha-status').addClass('error')
+					.text("Human verification Wrong!").fadeIn();
+				}
+			},
+			beforeSend: function() {
+				$('.captcha-status').text('Sending...');
+			}
+  		});
+	});
+});
+
+$.fn.centerCrop = function(){
+	$img = $(this);
+	$parent = $img.parent();
+
+	var cropCenter = function(){
+		$img.css({
+			'margin-left': 0.5 * ($parent.width() - $img.width())
+		});
+		$parent.css({
+			position: 'relative',
+			overflow: 'hidden'
+		});
+	}
+
+	var fullWidth = function(){
+		$img.css({
+			width: '100%'
+		});
+	}
+
+	var adjust = function (){
+		if ($img.width() < $parent.width()) {
+			fullWidth();
+		} else {
+			cropCenter();
+		}
+	}
+
+	$img.on('load', function(){
+		adjust();
+	});
+	
+	$(window).on('resize', function(){
+		adjust();
+	});
+}
---
+++ client/views/partials/faq.html
@@ -0,0 +1,64 @@
+<section id="faq" class="container-fluid text-center">
+	<div class="container">
+		<h2>FAQ</h2>
+		<p>PACKETS/AID STATIONS/POST RACE FESTIVITIES &amp; MORE </p>
+
+		<ul class="text-left" id="accordion">
+			<li>
+				<i class="fa fa-plus-circle"></i>
+				<a data-toggle="collapse" data-parent="#accordion" href="#collapse1">
+					Race packet filled with great goodies?
+				</a>
+				<p id="collapse1" class="panel-collapse collapse"> 
+					YES! 
+				</p>
+			</li>
+			<li>
+				<i class="fa fa-plus-circle"></i>
+				<a data-toggle="collapse" data-parent="#accordion" href="#collapse2">
+					FREE moisture wicking tech T-Shirt? 
+				</a>
+				<p id="collapse2" class="panel-collapse collapse"> 
+					 You got it! Hurry! Free t-shirt is limited to the first 100 registrants. And it is $12 afterwards, optional.  
+				</p>
+			</li>
+
+			<li>
+				<i class="fa fa-plus-circle"></i>
+				<a data-toggle="collapse" data-parent="#accordion" href="#collapse3">
+					Aid stations with drink &amp; snack? 
+				</a>
+				<p id="collapse3" class="panel-collapse collapse"> 
+					FIVE for the Half Marathon course! 
+				</p>
+			</li>
+			<li>
+				<i class="fa fa-plus-circle"></i>
+				<a data-toggle="collapse" data-parent="#accordion" href="#collapse4">
+					Cheers along the route? 
+				</a>
+				<p id="collapse4" class="panel-collapse collapse"> 
+					You will get A LOT!  
+				</p>
+			</li>
+			<li>
+				<i class="fa fa-plus-circle"></i>
+				<a data-toggle="collapse" data-parent="#accordion" href="#collapse5">
+					Free high resolution photos?  
+				</a>
+				<p id="collapse5" class="panel-collapse collapse"> 
+					WOW! 
+				</p>
+			</li>
+			<li>
+				<i class="fa fa-plus-circle"></i>
+				<a data-toggle="collapse" data-parent="#accordion" href="#collapse6">
+					Post race fun and festival for all ages? 
+				</a>
+				<p id="collapse6" class="panel-collapse collapse"> 
+					Absolutely! (Rumor is there will be special authentic food that you won't see at your ordinary post race!)  
+				</p>						
+			</li>
+		</ul>
+	</div>
+</section>
